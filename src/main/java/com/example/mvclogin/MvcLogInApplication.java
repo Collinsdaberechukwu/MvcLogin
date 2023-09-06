3f52291ccf564b790beaa016e0c8831083d7c8fc
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-09-06 05:32:45 +0100

    First Update

---
+++ src/main/java/com/example/mvclogin/MvcLogInApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvclogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcLogInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcLogInApplication.class, args);
+    }
+
+}
